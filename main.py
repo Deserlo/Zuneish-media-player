4499fc6fad340f2ac5540341c47245860b5604ac
eva <enajera58@gmail.com>
2020-09-04 22:15:45 -0700

    play pause unpause stop functionality
    loads and plays song from music dir

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import eel
+from pathlib import Path
 from mutagen import File
 from pygame import mixer
 
-file = r"C:\Users\18054\MyProjects\zune-ish\music\01 - Shak'em Loose Tonight.mp3"
+data_folder = Path(__file__).parent
+music_dir = data_folder / "music/"
+song_list = list(music_dir.rglob("*.mp3"))
+print(song_list)
+print(song_list[0])
+'''
+top_level_py_files = Path("src").glob("*.py")
+all_py_files = Path("src").rglob("*.py")
+'''
+file = song_list[0]
+filename = str(file)
+
 eel.init('web')
-
-
 
 @eel.expose
 def play_song():
@@ -13,7 +23,8 @@ def play_song():
     mixer.init() 
     
     # Loading the song 
-    mixer.music.load(file) 
+    print("Loading", filename)
+    mixer.music.load(filename) 
     
     # Setting the volume 
     mixer.music.set_volume(0.7) 
@@ -24,10 +35,17 @@ def play_song():
 @eel.expose
 def stop_song():
     mixer.music.stop()
+    mixer.music.unload()
 
 @eel.expose
 def pause_song():
+    print("pausing song..")
     mixer.music.pause()
+
+@eel.expose
+def unpause_song():
+    print("resuming song..")
+    mixer.music.unpause()
 
 eel.start('main.html', size=(800, 600), position=(0,0))
 
