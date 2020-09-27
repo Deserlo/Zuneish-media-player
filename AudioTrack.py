2a892b4d274f0061780310f07474b8c80ec540d8
eva <enajera58@gmail.com>
2020-09-26 22:53:51 -0700

    ui display

---
+++ AudioTrack.py
@@ -0,0 +1,4 @@
+class AudioTrack(object):
+    def __init__(self, name, file_path):
+        self.name = name
+        self.path = file_path
--- main.py
+++ main.py
@@ -1,39 +1,82 @@
 import eel
+from AudioTrack import AudioTrack
 from pathlib import Path
-from mutagen import File
+#from mutagen import File
 from pygame import mixer
+from jinja2 import Environment, FileSystemLoader
+from mutagen.id3 import ID3
+from mutagen.mp3 import MP3
+#from PIL import Image
 
+paused = False
+
+#Jinja template
+file_loader = FileSystemLoader('web/templates')
+env = Environment(loader=file_loader)
+template = env.get_template('index.html')
+
+
+
+#Directory to look for mp3 files
+data_folder = Path(__file__).parent
+music_dir = data_folder / "music/"
+song_paths = music_dir.rglob("*.mp3")
+
+
+#Mp3 Metadata
+def getMutagenTags(path):
+    """"""
+    audio = ID3(path)
+    #print(audio.pprint())
+    print ("Artist: %s" % audio['TPE1'].text[0])
+    print ("Album: %s" % audio['TALB'].text[0])
+    print ("Track: %s" % audio["TIT2"].text[0])
+    print ("Release Year: %s" % audio["TDRC"].text[0])
+
+
+
+#Storing list of AudioTrack objectors
+#  to output to template on starting Eel
+tracks = []
+artists = set()
+albums = set()
+for i in song_paths:
+    #getMutagenTags(i)
+    tags = ID3(i)
+    album = tags['TALB'].text[0]
+    artist = tags['TPE1'].text[0]
+    albums.add(album)
+    artists.add(artist)
+    track = AudioTrack(name=i.name, file_path=i)
+    tracks.append(track)
+
+
+#Jinja template rendering
+output = template.render(tracks=tracks, albums=albums, artists=artists)
+rendered_filename = data_folder / 'web' / 'templates' / 'main.html'
+with open(rendered_filename, "w") as f:
+    f.write(output)
 
 
 eel.init('web')
 
-
-paused = False
-data_folder = Path(__file__).parent
-music_dir = data_folder / "music/"
-song_paths = music_dir.rglob("*.mp3")
-song_list = []
-for i in song_paths:
-    song_list.append(i.name)
-
-
 @eel.expose
-def show_song():   
-    return song_list[0]
-
-@eel.expose
-def get_song_filename():
+def get_song(name):
     print ("Getting first song from directory...")
     '''
     top_level_py_files = Path("src").glob("*.py")
     all_py_files = Path("src").rglob("*.py")
     '''
-    file = song_list[0]
-    filename = str(file)
-    return filename
+    #dict {key=songName, value=songPath}
+    print(name)
+    for t in tracks:
+        if t.name == name:
+            filepath = t.path
+    print (filepath)
+    return str(filepath)
 
 @eel.expose
-def play_song():
+def play_song(songName):
     if paused:
         unpause_song()
     else:
@@ -41,8 +84,9 @@ def play_song():
         mixer.init() 
         
         # Loading the song 
-        print("Loading", get_song_filename())
-        mixer.music.load(get_song_filename()) 
+        song = get_song(songName)
+        print("Loading..", song)
+        mixer.music.load(song)
         
         # Setting the volume 
         mixer.music.set_volume(0.7) 
@@ -50,10 +94,13 @@ def play_song():
         # Start playing the song 
         mixer.music.play() 
 
+
 @eel.expose
 def stop_song():
     mixer.music.stop()
     mixer.music.unload()
+
+
 
 @eel.expose
 def pause_song():
@@ -63,6 +110,7 @@ def pause_song():
     mixer.music.pause()
     
 
+
 @eel.expose
 def unpause_song():
     global paused
@@ -71,5 +119,5 @@ def unpause_song():
     mixer.music.unpause()
     
 
-eel.start('index.html', size=(800, 600), position=(0,0))
+eel.start('templates/main.html', size=(800, 600))
 
