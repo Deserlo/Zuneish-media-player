a911293825271ac3b0824060825f56c9295f81ae
Deserlo <evamarienajera@csus.edu>
2021-01-17 01:36:59 -0800

    reformatted

--- sqllite/sqlCommands.py
+++ sqllite/sqlCommands.py
@@ -7,18 +7,16 @@ from classes.AudioTrack import AudioTrack
 from classes.Album import Album
 
 
-
 def get_sql_connection():
     try:
         conn = sqlite3.connect('music/sqllitedb/MusicLibrary.db')
         return conn
     except Error as e:
-        print(e)  
-    
+        print(e)
 
 
 def get_sql_query(name):
-    sql = { 
+    sql = {
         "now": """SELECT * FROM tracks WHERE id=?""",
         "next": """SELECT * FROM tracks WHERE id = (SELECT min(id) FROM tracks WHERE id > ?)""",
         "previous": """SELECT * FROM tracks WHERE id = (SELECT max(id) FROM tracks WHERE id < ?)""",
@@ -28,14 +26,16 @@ def get_sql_query(name):
     }
     return sql[name]
 
+
 def db_song_query(order, id):
     conn = get_sql_connection()
     conn.row_factory = sqlite3.Row
-    c = conn.cursor() 
+    c = conn.cursor()
     query = get_sql_query(order)
     c.execute(query, (id,))
     queryResult = c.fetchone()
-    id, path, track_name, album, artist = queryResult['id'], queryResult['path'], queryResult['track_name'], queryResult['album'], queryResult['artist']
+    id, path, track_name, album, artist = queryResult['id'], queryResult[
+        'path'], queryResult['track_name'], queryResult['album'], queryResult['artist']
     c.close()
     return [id, path, track_name, album, artist]
 
@@ -46,12 +46,13 @@ def db_load_all_songs():
     albums = set()
     conn = get_sql_connection()
     conn.row_factory = sqlite3.Row
-    c = conn.cursor() 
+    c = conn.cursor()
     query = get_sql_query('all')
     c.execute(query)
     queryResults = c.fetchall()
-    for row in queryResults:  
-        track = AudioTrack(id=row[4], name=row[0], album=row[2], artist=row[3], file_path=row[1].replace("\\", "\\\\"))
+    for row in queryResults:
+        track = AudioTrack(id=row[4], name=row[0], album=row[2],
+                           artist=row[3], file_path=row[1].replace("\\", "\\\\"))
         tracks.append(track)
         album = Album(name=row[2], artist=row[3], img=row[2] + ".thumbnail")
         albums.add(album)
@@ -68,7 +69,8 @@ def db_get_player_status():
     c.execute(query)
     queryResult = c.fetchone()
     c.close()
-    id, path, track_name, album, artist = queryResult['id'], queryResult['path'], queryResult['track_name'], queryResult['album'], queryResult['artist']
+    id, path, track_name, album, artist = queryResult['id'], queryResult[
+        'path'], queryResult['track_name'], queryResult['album'], queryResult['artist']
     return [id, path, track_name, album, artist]
 
 
@@ -77,9 +79,6 @@ def db_update_player_status(values):
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
     stmt = get_sql_query("update_player")
-    c.execute(stmt,(values[0], values[1], values[2], values[3], values[4]))
+    c.execute(stmt, (values[0], values[1], values[2], values[3], values[4]))
     conn.commit()
     c.close()
-    
-
-
